e0605ef61052fa985867b713172cb24f1dc5cd13
rocioarias <rocioarias888@gmail.com>
2024-01-06 12:32:12 -0300

    modifications of music

--- src/logic/music.js
+++ src/logic/music.js
@@ -3,42 +3,44 @@ import YouTubePlayer from "react-player/youtube";
 import "../visual/music.css"; // Ensure this path is correct
 
 function MusicReproduction() {
-    const [playing, setPlaying] = useState(false);
+    const [playing, setPlaying] = useState(true);
     const [volume, setVolume] = useState(0.25);
 
     const handlePlay = () => {
         setPlaying(!playing);
     }
 
-    const changeVolume = (rule) => {
-        if(rule === '-'){
-            setVolume(volume-0.1);
-        }
-        else{
-            setVolume(volume+0.1);
-        }
-    }
 
     return (
         <div className="py-5">
             <div className="mp3Reproducer">
                 <div className="mp3Reproducer-screen">
                     <YouTubePlayer
-                        url="https://youtu.be/qdDVtFvJwUc?list=RDqdDVtFvJwUc"
+                        url="https://youtu.be/L_StmRnuREw"
                         controls={false}
                         volume={volume}
-                        loop
-                        
                         className="video"
                         playing={playing}
                     />
                 </div>
+                <input
+                        type="range"
+                        min={0}
+                        max={1}
+                        step={0.02}
+                        value={volume}
+                        className="mp3Reproducer-volume"
+                        onChange={event => {
+                            setVolume(event.target.valueAsNumber)
+                        }}
+                />
                 <div className="mp3Reproducer-circle">
-                    <button className="mp3Reproducer-button volume-up" onClick={() => changeVolume("+")}>+</button>
-                    <button className="mp3Reproducer-button play" onClick={handlePlay}>
-                        {playing ? 'Pause' : 'Play'}
-                    </button>
-                    <button className="mp3Reproducer-button volume-down" onClick={() => changeVolume("-")}>-</button>
+                    
+
+                    <button className={playing ? "mp3Reproducer-button pause" : "mp3Reproducer-button play"} onClick={handlePlay} />
+                        
+                    
+
                 </div>
             </div>
         </div>
